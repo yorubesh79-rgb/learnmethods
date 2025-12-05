35c8d2f89fd9ce6cd4ce3ce1a6470d5fc96d09cb
yorubesh79-rgb <yorubesh79@gmail.com>
2025-12-05 10:54:45 +0530

    Add files via upload

---
+++ LearnMethods.java
@@ -0,0 +1,16 @@
+package learnautomation1;
+
+ public class LearnMethods {
+	 
+	  public void student() {
+		System.out.println("rubesh");
+	  }
+
+	public static void main(String[] args) {
+		LearnMethods s1=new LearnMethods();
+		s1.student();
+		// TODO Auto-generated method stub
+
+	}
+
+}
